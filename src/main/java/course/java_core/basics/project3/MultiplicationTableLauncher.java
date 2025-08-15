8cf3c49021bfc1f490c7e7e29892f53aab1b3fe6
radcheenkoo <radcenkodmitro39@gmail.com>
2025-08-15 14:27:13 +0300

    Multiplication table relesed

---
+++ src/main/java/course/java_core/basics/project3/MultiplicationTableLauncher.java
@@ -0,0 +1,16 @@
+package course.java_core.basics.project3;
+
+public class MultiplicationTableLauncher {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+
+                System.out.printf(i + " * " + j + " = " + i * j + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
